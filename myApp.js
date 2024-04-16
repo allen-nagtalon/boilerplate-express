6b211c44705291c726b3b87491babf2f14f0cd29
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-04-16 14:50:42 -0700

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ console.log('Hello World');
 
 app.use('/public', express.static('public'))
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
