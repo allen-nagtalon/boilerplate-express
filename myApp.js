ebd59f2da81df0c870dd8acda4ececcff41c1b5c
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-04-16 14:37:18 -0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
 console.log('Hello World');
 
+app.use('/public', express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 module.exports = app;
